87f48d9c3b48f0720712c83f6e4f454a7e752ba5
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2025-04-20 18:17:37 -0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,3 +1,18 @@
 import streamlit as st
 
-st.title("Crm para oficinas")
+def main():
+    st.set_page_config(page_title="Simple CRM", layout="wide")
+    st.title("Simple CRM")
+
+    # Sidebar for navigation
+    page = st.sidebar.selectbox("Navigation", ["Dashboard", "Adicionar prospecto", "Ver prospecto"])
+    if page == "Dashboard":
+        st.title("Dashboard")
+    elif page == "Adicionar prospecto":
+        st.title("Adicionar prospecto")
+    elif page == "Ver prospecto":
+        st.title("Ver prospecto")
+
+
+if __name__ == "__main__":
+    main() 
